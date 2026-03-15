e410730bb7ea91ee9ae4564e799de43beab074ea
QDYG <thanglinh868686@gmail.com>
2026-03-16 01:35:05 +0700

    Add error handling to parser with Result based iteration

--- src/parser.rs
+++ src/parser.rs
@@ -1,85 +1,119 @@
-use crate::token::Token;
+use crate::error_handler::{Error, ErrorType};
 use crate::node::Node;
+use crate::token::{Token, TokenType};
 use crate::value::Value;
 
-pub struct Parser<L: Iterator<Item=Token>> {
+pub struct Parser<L>
+where
+    L: Iterator<Item = Result<Token, Error>>,
+{
     lex: L,
     tokens: Vec<Token>,
-    pos: usize
+    pos: usize,
+    errors: Vec<Error>,
 }
 
-impl<L: Iterator<Item=Token>> Parser<L> {
-    pub fn new(lex: L) -> Self { Self{ lex, tokens: Vec::new(), pos: 0 } }
+impl<L> Parser<L>
+where
+    L: Iterator<Item = Result<Token, Error>>,
+{
+    pub fn new(lex: L) -> Self {
+        Self {
+            lex,
+            tokens: Vec::new(),
+            pos: 0,
+            errors: Vec::new(),
+        }
+    }
 
     fn ensure_buffer(&mut self) -> bool {
         while self.pos >= self.tokens.len() {
             match self.lex.next() {
                 Some(tokens) => {
-                    self.tokens.push(tokens);
-                    if !self.tokens.is_empty() { return true }
+                    match tokens {
+                        Ok(tokens) => self.tokens.push(tokens),
+                        Err(error) => self.errors.push(error),
+                    }
+                    if !self.tokens.is_empty() {
+                        return true;
+                    }
                 }
-                None => return false
+                None => return false,
             }
         }
         true
     }
 
     fn advance(&mut self, steps: usize) -> Option<Token> {
-        let character = self.peek();
-        if character.is_some() { self.pos += steps }
-        character
+        if !self.ensure_buffer() {
+            return None;
+        }
+        let token = Some(self.tokens[self.pos].clone());
+        self.pos += steps;
+        token
     }
 
     fn peek(&mut self) -> Option<Token> {
         if self.ensure_buffer() {
             Some(self.tokens[self.pos].clone())
-        } else { None }
+        } else {
+            None
+        }
     }
 
     fn to_node(&mut self, token: Token) -> Node {
-        match token {
-            Token::Int(number) => Node::Literal(Value::Integer(number)),
-            Token::Float(number) => Node::Literal(Value::Float(number)),
-            Token::Identifier(identifier) => self.parse_function(identifier),
-            _ => todo!("Unimplemented token: {:?}", token)
+        match token.kind {
+            TokenType::Int(number) => Node::Literal(Value::Integer(number)),
+            TokenType::Float(number) => Node::Literal(Value::Float(number)),
+            TokenType::Identifier(identifier) => self.parse_function(identifier),
+            TokenType::String(string) => Node::Literal(Value::String(string)),
+            _ => todo!("Unimplemented token: {:?}", token),
         }
     }
 
     fn parse_function(&mut self, token: String) -> Node {
         let operator = Box::new(Node::Symbol(token));
-        if self.advance(1) != Some(Token::LeftParen) {
+        if let Some(token) = self.advance(1) && token.kind != TokenType::LeftParen {
             todo!("Behind operator need a left paren!");
         }
         let mut arguments = Vec::new();
         while let Some(argument) = self.advance(1) {
-            if argument == Token::RightParen { break }
+            if argument.kind == TokenType::RightParen {
+                break;
+            }
             arguments.push(self.to_node(argument));
         }
-        self.advance(1); // Skip right paren
-        Node::Apply { operator, arguments }
+        Node::Apply {operator, arguments}
     }
 
-    fn parse_pipeline(&mut self, token: Token) -> Node {
+    fn parse_pipeline(&mut self, token: Token) -> Result<Node, Error> {
         let mut stations = Vec::new();
         stations.push(self.to_node(token));
-        if self.peek().is_none() || self.peek().unwrap() != Token::Arrow {
+        if self.peek().is_none() || self.peek().unwrap().kind != TokenType::Arrow {
             match stations.pop() {
-                Some(station) => return station,
-                _ => todo!("There is no station before pipeline!")
+                Some(station) => return Ok(station),
+                _ => todo!("There is no station before pipeline!"),
             }
         }
-        while let Some(Token::Arrow) = self.peek() {
+        while let Some(token) = self.peek() && token.kind == TokenType::Arrow {
             self.advance(1);
             if let Some(token) = self.advance(1) {
                 stations.push(self.to_node(token));
-            } else { todo!("We should make error_handle.rs") }
+            } else {
+                todo!("We should make error_handle.rs")
+            }
         }
-        Node::Pipeline(stations)
+        Ok(Node::Pipeline(stations))
     }
+
+    fn error_pusher() {}
 }
 
-impl<L: Iterator<Item=Token>> Iterator for Parser<L> {
-    type Item = Node;
+impl<L> Iterator for Parser<L>
+where
+    L: Iterator<Item = Result<Token, Error>>,
+{
+    type Item = Result<Node, Error>;
     fn next(&mut self) -> Option<Self::Item> {
         self.advance(1).map(|token| self.parse_pipeline(token))
     }
