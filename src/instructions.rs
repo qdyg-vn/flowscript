fbb24eed120aef203d09259bfd442f2527f2a3b7
QDYG <thanglinh868686@gmail.com>
2026-03-19 12:25:24 +0700

    Add symbol index to Call instruction and implement specialized instructions for builtin/global calls

--- src/instructions.rs
+++ src/instructions.rs
@@ -2,11 +2,14 @@ use crate::value::Value;
 
 #[derive(Debug)]
 pub enum Instruction {
-    Call(u16),
+    Call(u16, u16),
     Load(u16),
     Store(u16),
+    BuiltinCall(u16),
+    GlobalCall(u16)
 }
 
+#[derive(Debug)]
 pub struct Chunk {
     pub instructions: Vec<Instruction>,
     pub constants: Vec<Value>,
