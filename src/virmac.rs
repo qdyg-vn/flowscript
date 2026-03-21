0bb37ff50a9ef3c057d0958852b01fed2448af05
QDYG <thanglinh868686@gmail.com>
2026-03-22 06:00:02 +0700

    Optimize VirMac stack management with pre-allocated array and position tracking

--- src/virmac.rs
+++ src/virmac.rs
@@ -3,23 +3,27 @@ use crate::instructions::{Chunk, Instruction};
 use crate::value::Value;
 
 pub struct VirMac {
-
+    postion: usize
 }
 
 impl VirMac {
     pub fn new() -> Self {
-        Self {}
+        Self { postion: 0 }
     }
 
     pub fn execute(&mut self, map: Vec<Chunk>) -> Vec<Value> {
-        let mut stack = Vec::new();
+        let mut stack = vec![Value::Nil; 1024];
         for chunk in map {
             for instruction in chunk.instructions {
                 match instruction {
-                    Instruction::Load(index) => stack.push(chunk.constants[index as usize].clone()),
+                    Instruction::Load(index) => {
+                        if stack.len() <= self.postion { stack.resize(stack.len() * 2, Value::Nil) }
+                        stack[self.postion] = chunk.constants[index as usize].clone();
+                        self.postion += 1
+                    },
                     Instruction::BuiltinCall(index, arity) => {
-                        let start = stack.len() - arity as usize;
-                        let end = stack.len() - 1;
+                        let start = self.postion - arity as usize;
+                        let end = self.postion - 1;
                         self.execute_builtin_function(index, &mut stack, start, end);
                     }
                     _ => todo!()
@@ -29,13 +33,16 @@ impl VirMac {
         stack
     }
 
-    fn execute_builtin_function(&self, index: u16, stack: &mut Vec<Value>, start: usize, end: usize) {
+    fn execute_builtin_function(&mut self, index: u16, stack: &mut Vec<Value>, start: usize, end: usize) {
         if let Some(arguments) = stack.get(start..=end) {
             let builtin = get_builtin(index);
             match builtin.function {
                 BuiltinFunction::Math(function) => {
                     match function(arguments) {
-                        Ok(value) => stack.push(value),
+                        Ok(value) => {
+                            stack[start] = value;
+                            self.postion = start
+                        },
                         Err(error) => todo!()
                     };
                 },
