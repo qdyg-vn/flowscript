46fa730874c0d8aae7a4478236f7b4a892088ab7
QDYG <thanglinh868686@gmail.com>
2026-05-04 18:42:51 +0700

    Introduce unified Value enum for runtime values

--- src/value.rs
+++ src/value.rs
@@ -5,7 +5,7 @@ use std::rc::Rc;
 use crate::instructions::Chunk;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
-pub enum Value {
+pub enum LightValue {
     Boolean(bool),
     Nil,
     Float(f64),
@@ -14,6 +14,71 @@ pub enum Value {
     FunctionPointer(u32),
     ClosurePointer(u32),
     ArrayPointer(u32),
+}
+
+impl LightValue {
+    pub const BOOLEAN: u8 = 0;
+    pub const NIL: u8 = 1;
+    pub const FLOAT: u8 = 2;
+    pub const INTEGER: u8 = 3;
+    pub const STRINGPOINTER: u8 = 4;
+    pub const FUNCTIONPOINTER: u8 = 5;
+    pub const ARRAYPOINTER: u8 = 6;
+    pub fn to_byte(&self) -> Vec<u8> {
+        match self {
+            LightValue::Boolean(boolean) => vec![*boolean as u8],
+            LightValue::Nil => vec![0],
+            LightValue::Float(float) => float.to_le_bytes().to_vec(),
+            LightValue::Integer(integer) => integer.to_le_bytes().to_vec(),
+            LightValue::StringPointer(index) | LightValue::FunctionPointer(index) | LightValue::ArrayPointer(index) => index.to_le_bytes().to_vec(),
+            _ => unreachable!()
+        }
+    }
+}
+
+impl Eq for LightValue {}
+
+impl Hash for LightValue {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        match self {
+            LightValue::Boolean(boolean) => boolean.hash(state),
+            LightValue::Integer(integer) => integer.hash(state),
+            LightValue::Float(float) => float.to_bits().hash(state),
+            LightValue::StringPointer(string) => string.hash(state),
+            LightValue::Nil => 0.hash(state),
+            _ => unreachable!(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum HeavyValue {
+    String(String),
+    Function(Chunk),
+    Closure(Closure),
+    Array(Vec<LightValue>),
+}
+
+impl Eq for HeavyValue {}
+
+impl Hash for HeavyValue {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        match self {
+            HeavyValue::String(string) => string.hash(state),
+            _ => unreachable!(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Value {
+    Boolean(bool),
+    Nil,
+    Float(f64),
+    Integer(i64),
+    String(String),
+    Function(Vec<u8>),
+    Array(Vec<Value>),
 }
 
 impl fmt::Display for Value {
@@ -28,61 +93,14 @@ impl fmt::Display for Value {
     }
 }
 
-impl Value {
-    pub fn to_byte(&self) -> Vec<u8> {
-        match self {
-            Value::Boolean(boolean) => vec![*boolean as u8],
-            Value::Nil => vec![0],
-            Value::Float(float) => float.to_le_bytes().to_vec(),
-            Value::Integer(integer) => integer.to_le_bytes().to_vec(),
-            Value::StringPointer(index) | Value::FunctionPointer(index) | Value::ArrayPointer(index) => index.to_le_bytes().to_vec(),
-            _ => unreachable!()
-        }
-    }
-}
-
-impl Eq for Value {}
-
-impl Hash for Value {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        match self {
-            Value::Boolean(boolean) => boolean.hash(state),
-            Value::Integer(integer) => integer.hash(state),
-            Value::Float(float) => float.to_bits().hash(state),
-            Value::StringPointer(string) => string.hash(state),
-            Value::Nil => 0.hash(state),
-            _ => unreachable!(),
-        }
-    }
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub enum HeavyValue {
-    String(String),
-    Function(Chunk),
-    Closure(Closure),
-    Array(Vec<Value>),
-}
-
-impl Eq for HeavyValue {}
-
-impl Hash for HeavyValue {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        match self {
-            HeavyValue::String(string) => string.hash(state),
-            _ => unreachable!(),
-        }
-    }
-}
-
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub enum Upvalue {
     Open(usize),
-    Closed(Value)
+    Closed(LightValue)
 }
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Closure {
-    pub function: Rc<Value>,
+    pub function: Rc<LightValue>,
     pub upvalue: Vec<Rc<RefCell<Upvalue>>>,
 }
